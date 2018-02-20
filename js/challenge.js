0d7251663d1d0fc95dba3423c3c1704db80d494d
Damien Gilles <damien.gilles@alterway.fr>
2018-02-20 15:35:14 +0100

    challenging google bot

--- js/challenge.js
+++ js/challenge.js
@@ -1,51 +1,51 @@
 window.onload = () => {
-    document.getElementById("3").innerHTML("3. window.onlaod");
-}
-
-document.onload = () => {
-    document.getElementById("4").innerHTML("4. dociment.onlaod");
+    document.getElementById("3").innerHTML = "3. window.onlaod";
 }
 
 (function() {
-    document.getElementById("5").innerHTML("5. seaf");
+    window.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("4").innerHTML = "4. seaf + DOMContentLoaded";
+    });
+})();
+
+(function() {
+    document.getElementById("5").innerHTML = "5. seaf";
 })();
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("6").innerHTML("6. DOMContentLoaded");
+    document.getElementById("6").innerHTML = "6. DOMContentLoaded";
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("7").innerHTML("7. DOMContentLoaded fleched");
+    document.getElementById("7").innerHTML = "7. DOMContentLoaded fleched";
 });
 
 window.addEventListener("load", function() {
-    document.getElementById("8").innerHTML("8. load");
+    document.getElementById("8").innerHTML = "8. load";
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("9").innerHTML("9. load fleched");
+    document.getElementById("9").innerHTML = "9. load fleched";
 });
 
 (function() {
     window.addEventListener("load", function() {
-        document.getElementById("10").innerHTML("10. seaf + load");
+        document.getElementById("10").innerHTML = "10. seaf + load";
     });
 })();
 
 setTimeout(() => {
     (function() {
         window.addEventListener("load", function() {
-            document.getElementById("11").innerHTML("11. async + seaf + load");
+            document.getElementById("11").innerHTML = "11. async + seaf + load";
         });
     })();
 }, 0);
 
 setTimeout(() => {
-    setTimeout(() => {
-        (function() {
-            window.addEventListener("load", function() {
-                document.getElementById("12").innerHTML("12. async*2 + seaf + load");
-            });
-        })();
-    }, 0);
+    (function() {
+        window.addEventListener("DOMContentLoaded", function() {
+            document.getElementById("12").innerHTML = "12. async + seaf + DOMContentLoaded";
+        });
+    })();
 }, 0);
